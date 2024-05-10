23aab6af257dd4175ee37d6fad00006822764068
Brendan <brendan.l.martin@gmail.com>
2024-05-10 16:58:41 +0200

    User can now be found on the map to display nearest

--- app/src/main/java/com/example/mysdfapp/MainActivity.java
+++ app/src/main/java/com/example/mysdfapp/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.mysdfapp;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,27 +14,28 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.mysdfapp.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.firestore.Query;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    public enum ResearchOption{
+    public enum ResearchOption {
         POPULAR,
         NEAREST,
         KEYWORDS
@@ -39,36 +46,45 @@ public class MainActivity extends AppCompatActivity {
 
     private List<String> _researchOptionsLabels;
     private List<String> _categories;
-    public List<String> getCategories(){
+
+    public List<String> getCategories() {
         return _categories;
     }
+
     private ActivityMainBinding _binding;
     private FragmentManager _fragmentManager;
     private DatabaseManager _databaseManager;
-    public DatabaseManager getDatabaseManager(){
+
+    public DatabaseManager getDatabaseManager() {
         return _databaseManager;
     }
+
+    private LocationManager _locationManager;
+    private GeoPoint _userLocation;
 
     // Main layout ui component
     private Spinner _researchSpinner;
     private Spinner _keywordSpinner;
     private Toolbar _toolbar;
     private FloatingActionButton _myspaceButton;
-    public void showFloatingButton(boolean enabled){
-        if (enabled){
+
+    public void showFloatingButton(boolean enabled) {
+        if (enabled) {
             _myspaceButton.setVisibility(View.VISIBLE);
-        }
-        else{
+        } else {
             _myspaceButton.setVisibility(View.GONE);
         }
     }
-    public void FloatingButtonSwitchToAddMode(){
+
+    public void FloatingButtonSwitchToAddMode() {
         _myspaceButton.setOnClickListener(_addListener);
     }
-    public void FloatingButtonSwitchToMySpaceMode(){
+
+    public void FloatingButtonSwitchToMySpaceMode() {
         _myspaceButton.setOnClickListener(_myspaceListener);
     }
-    public void setFloatingButtonIcon(int resID){
+
+    public void setFloatingButtonIcon(int resID) {
         _myspaceButton.setImageResource(resID);
     }
 
@@ -95,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         _binding = ActivityMainBinding.inflate(getLayoutInflater());
         _fragmentManager = getSupportFragmentManager();
         _databaseManager = new DatabaseManager();
+        _locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         setContentView(_binding.getRoot());
 
@@ -105,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(_toolbar);
 
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             substituteFragment(new PostListFragment());
         }
 
@@ -120,27 +137,27 @@ public class MainActivity extends AppCompatActivity {
         _researchSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
+                switch (position) {
                     case 0:
-                        if (_currentOption == ResearchOption.KEYWORDS){
+                        if (_currentOption == ResearchOption.KEYWORDS) {
                             hideKeywordsOptions();
                         }
-                        if (_currentOption != ResearchOption.POPULAR){
+                        if (_currentOption != ResearchOption.POPULAR) {
                             _databaseManager.ResetLastDocument();
                             _currentOption = ResearchOption.POPULAR;
                         }
                         break;
                     case 1:
-                        if (_currentOption == ResearchOption.KEYWORDS){
+                        if (_currentOption == ResearchOption.KEYWORDS) {
                             hideKeywordsOptions();
                         }
-                        if (_currentOption != ResearchOption.NEAREST){
+                        if (_currentOption != ResearchOption.NEAREST) {
                             _databaseManager.ResetLastDocument();
                             _currentOption = ResearchOption.NEAREST;
                         }
                         break;
                     case 2:
-                        if (_currentOption != ResearchOption.KEYWORDS){
+                        if (_currentOption != ResearchOption.KEYWORDS) {
                             showKeywordsOptions();
                             _databaseManager.ResetLastDocument();
                             _currentOption = ResearchOption.KEYWORDS;
@@ -174,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void applyToCategories(List<String> categories) {
                 String log = "Categories fetched: ";
-                for (String category : categories){
+                for (String category : categories) {
                     _categories.add(category);
                     log += category + " ";
                 }
@@ -183,30 +200,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
         _myspaceButton.setOnClickListener(_myspaceListener);
+
+        if (ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Demander la permission
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_COARSE_LOCATION}, 12345);
+        }
     }
 
-    private void substituteFragment(Fragment fragment){
+    private void substituteFragment(Fragment fragment) {
         Log.i("MainActivity", "substituteFragment()");
         _fragmentManager.beginTransaction()
                 .replace(R.id.main_fragment_container, fragment)
                 .commit();
     }
 
-    private void commitFragment(Fragment fragment, String tag){
+    private void commitFragment(Fragment fragment, String tag) {
         _fragmentManager.beginTransaction()
                 .replace(R.id.main_fragment_container, fragment)
                 .addToBackStack(tag)
                 .commit();
     }
 
-    public void setToolbarTitle(String title){
+    public void setToolbarTitle(String title) {
         _toolbar.setTitle(title);
     }
 
-    public void setBackButtonEnabled(boolean enabled){
+    public void setBackButtonEnabled(boolean enabled) {
         getSupportActionBar().setDisplayHomeAsUpEnabled(enabled);
     }
 
@@ -224,7 +245,7 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == android.R.id.home){
+        if (id == android.R.id.home) {
             if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
                 // S'il y a des fragments dans la pile de retour arrière, revenir au précédent
                 getSupportFragmentManager().popBackStack();
@@ -235,17 +256,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void showKeywordsOptions(){
+    public void showKeywordsOptions() {
         _keywordSpinner.setVisibility(View.VISIBLE);
     }
 
-    public void hideKeywordsOptions(){
+    public void hideKeywordsOptions() {
         _keywordSpinner.setVisibility(View.GONE);
     }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Application doesn't have the permission to get the location.", Toast.LENGTH_SHORT).show();
+        }
+        Location location = _locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            _userLocation = new GeoPoint(latitude, longitude);
+        }
+    }
+
 
     public void fetchInDatabase(DatabaseManager.ExecuteToListAfterQueryAction action){
         switch (_currentOption){
@@ -253,8 +286,13 @@ public class MainActivity extends AppCompatActivity {
                 _databaseManager.searchAnnouncementsOrderByLikes(action);
                 break;
             case NEAREST:
-                GeoPoint localisation = null;
-                _databaseManager.searchAnnouncementsOrderByProximity(action, localisation);
+                getLastLocation();
+                if (_userLocation != null){
+                    _databaseManager.searchAnnouncementsOrderByProximity(action, _userLocation);
+                }
+                else{
+                    Toast.makeText(this, "Couldn't find the users location.", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case KEYWORDS:
                 _databaseManager.searchAnnouncementsByCategory(_currentCategory, action);
