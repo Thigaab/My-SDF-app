6960b688014691455345a9aea630ce167fe75c30
Thibaut BONEFONT <thibaut.bonefont@epita.fr>
2024-05-10 18:36:27 +0200

    detailedLayout is now a Fragment

--- app/src/main/java/com/example/mysdfapp/DetailedLayout.java
+++ app/src/main/java/com/example/mysdfapp/DetailedLayout.java
@@ -1,40 +1,44 @@
 package com.example.mysdfapp;
 
 import android.os.Bundle;
-import android.service.quicksettings.Tile;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class DetailedLayout extends AppCompatActivity {
-    Announcement Announce;
+public class DetailedLayout extends Fragment {
+    private Announcement Announce;
 
-    DetailedLayout(Announcement announce){
+    public DetailedLayout(Announcement announce) {
         Announce = announce;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.details_layout);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.details_layout, container, false);
 
-        TextView titleTextView = findViewById(R.id.details_title);
-        TextView descriptionTextView = findViewById(R.id.details_description);
-        TextView likesTextView = findViewById(R.id.details_likes);
-        TextView endDateTextView = findViewById(R.id.details_endDate);
-
+        TextView titleTextView = rootView.findViewById(R.id.details_title);
+        TextView descriptionTextView = rootView.findViewById(R.id.details_description);
+        TextView likesTextView = rootView.findViewById(R.id.details_likes);
+        TextView endDateTextView = rootView.findViewById(R.id.details_endDate);
 
         titleTextView.setText(Announce.Title);
         descriptionTextView.setText(Announce.Description);
-        likesTextView.setText(Math.toIntExact(Announce.Number_of_likes));
+        likesTextView.setText(String.valueOf(Announce.Number_of_likes));
         endDateTextView.setText(Announce.End.toString());
 
         MapsFragment mapsFragment = new MapsFragment();
 
-        getSupportFragmentManager().beginTransaction()
+        getChildFragmentManager().beginTransaction()
                 .replace(R.id.mapFragment, mapsFragment)
                 .commit();
 
         mapsFragment.setDestination(Announce.Coordinates.getLatitude(), Announce.Coordinates.getLongitude(), Announce.Title);
+
+        return rootView;
     }
 }
